57ac713c0b571f85e2a8d1ddc680606ba0261c80
notaika <ercamanalo@gmail.com>
2024-05-19 00:02:21 -0700

    create: started making MVP functionality of Ha.BIT fix: edited some styles and spacing - minor bug fixes in the UI added: added more sprites and assets

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import coin from '../../assets/logos/coin.webp'
+import coin from '../../assets/logos/coin.gif'
 import './Navbar.scss'
 
 export default function Navbar() {
   return (
     <div className="navbar">
-        <img src={coin} alt="Ha.BIT Logo - a coin" className="navbar__logo" />
+        <Link to='/'><img src={coin} alt="Ha.BIT Logo - a coin" className="navbar__logo" /></Link>
         <div className="navbar__links">
             <Link to='/' className="navbar__links-item">Home</Link>
             <Link to='/' className="navbar__links-item">About</Link>
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,8 +3,30 @@ import './Dashboard.scss'
 
 export default function Dashboard() {
   return (
-    <div>
-      
-    </div>
+    <main className="dashboard">
+      <article className="dashboard__center">
+        <div className="dashboard__row">
+          <div className="dashboard__row-top--left"></div>
+          <div className="dashboard__row-top--right">
+            <div className="dashboard__row-player">
+              <div className="dashboard__row-player--left">
+                <p className="dashboard__row-player-name">Welcome, Traveler</p>
+                <p className="dashboard__row-player-status">CURRENTLY: Deployed</p>
+                <p className="dashboard__row-player-location">LOCATION: On an expedition</p>
+                <p className="dashboard__row-player-reputation">REPUTATION: 128 hours</p>
+              </div>
+              <div className="dashboard__row-player--right">
+                <p className="dashboard__row-player-level">Select an expedition:</p>
+                <div className="dashboard__row-player-select"> MAP THROUGH LEVELS HERE FROM API</div>
+              </div>
+            </div>
+            <div className="dashboard__row-timer"></div>
+          </div>
+        </div>
+        <div className="dashboard__row"></div>
+        <div className="dashboard__row"></div>
+        <div className="dashboard__row"></div>
+      </article>
+    </main>
   )
 }
